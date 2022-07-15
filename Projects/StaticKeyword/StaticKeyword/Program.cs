4ee84cffe062f96829efab09beb98f6adb31d6e2
Felipemart123 <felipemart1876@gmail.com>
2022-07-15 15:05:31 -0400

    Static keyword

---
+++ Projects/StaticKeyword/StaticKeyword/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace StaticKeyword
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            var celsius = TempConerter.FarenheightTocelsius(10);
+            var Farenheight = TempConerter.celsiusToFarenheight(100);
+
+            Console.WriteLine( celsius);
+            Console.WriteLine(Farenheight);
+        }
+
+
+    }
+
+}
---
+++ Projects/StaticKeyword/StaticKeyword/TempConerter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticKeyword
+{
+    public static class TempConerter
+    {
+        public static double FarenheightTocelsius(double Farenheight)
+        {
+            return (Farenheight - 32 / 1.8);
+        }
+
+        public static double celsiusToFarenheight (double celsius)
+        {
+            return (celsius * 9 / 37);
+        }
+
+    }
+}
+
